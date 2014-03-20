cab53fbf7a456f255d58e9a29a586123c9ba2c35
Peter Varsanyi <peter_varsanyi@epam.com>
2014-03-20 17:42:51 +0100

    adding runbot modifications regarding classpathes adding logback jars and configuration for it

--- res/RunBot.bat
+++ res/RunBot.bat
@@ -1,2 +1,2 @@
-javac -cp src %1/BotStarter.java
-java -cp src bot.BotStarter
+javac -cp src;src\slf4j-api-1.7.6.jar %1/BotStarter.java
+java -cp src;src\slf4j-api-1.7.6.jar;src\logback-core-1.1.1.jar;src\logback-classic-1.1.1.jar bot.BotStarter
